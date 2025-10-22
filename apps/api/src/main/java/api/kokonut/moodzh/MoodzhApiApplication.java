8a1b57ad68a57c47652842b7f92672e8f1010cb5
kokonut <castaneda.jair.13@gmail.com>
2025-10-22 00:09:05 -0500

    chore(root): name changes

--- apps/api/src/main/java/api/kokonut/moodzh/MoodzhApiApplication.java
+++ apps/api/src/main/java/api/kokonut/moodzh/MoodzhApiApplication.java
@@ -1,4 +1,4 @@
-package api.kokostore.moodzh;
+package api.kokonut.moodzh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
